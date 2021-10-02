b9bfc41647ca3ef25a32b898631c67adc862824c
taren4ik <taren4ik@mail.ru>
2021-10-02 16:46:34 +1000

    Убрал SQalchemy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect   #render выводит страницы
+from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
 
 app = Flask(__name__)
