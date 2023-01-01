6989f6e6151c11aa932da57bc1f202efc585490c
Dmitry Permyakov <taren4ik@mail.ru>
2023-01-01 22:30:17 +1000

    исправил *.app

---
+++ comp_intel/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/photos')
+def photos():
+    return render_template('photos.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
