e71e266a979f4c2cb91ad7929f17497895b0b3ea
Dmitry Permyakov <taren4ik@mail.ru>
2024-02-24 01:58:26 +1000

    Добавлена добавлены ссылки на отчеты и бот. Скорректировал код

--- comp_intel/app.py
+++ comp_intel/app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from flask import Flask, render_template, url_for, request, redirect, flash
 from dotenv import load_dotenv
@@ -11,10 +12,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 
-async def async_report(*args):
-    """Создает отчет по чату телеграм."""
-    pass
-
+@app.context_processor
+def year():
+    datetime_now = datetime.datetime.now()
+    return {
+        'year': datetime_now.year,
+    }
 
 @app.route('/')
 @app.route('/home')
@@ -22,9 +25,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/photos')
-def photos():
-    return render_template('photos.html')
+@app.route('/books')
+def books():
+    return render_template('books.html')
 
 
 @app.route('/about')
@@ -42,28 +45,9 @@ def contact():
     return render_template('contact.html')
 
 
-@app.route('/services', methods=['GET', 'POST'])
-async def services():
-    data = await async_report()
-
-    channel = request.form.get('channel')
-    check_users = request.form.get('users')
-    check_messages = request.form.get('messages')
-
-    if request.method == 'POST':
-
-        if not(channel):
-            flash('Заполните поле для проверки чата')
-            return render_template('services.html', )
-
-    else:
-        flash('Для получения информации по группе заполните форму.')
+@app.route('/services')
+def services():
     return render_template('services.html')
-
-
-@app.route('/source')
-def source():
-    return render_template('source.html')
 
 
 if __name__ == '__main__':
